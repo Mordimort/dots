cf6cdf2a6560c526457261e041f453f6ef9093b9
Mordimort <104747947+Mordimort@users.noreply.github.com>
2026-06-13 15:33:51 +0800

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+fish_add_path /home/miyo/.spicetify
+set -gx fish_prompt_pwd_dir_length 0
